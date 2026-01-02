a74afbd136dc6a087501e6701981ce8c36dab0a9
Mazhar Khan <thehadit26@gmail.com>
2026-01-02 15:13:12 +0530

    Added Python variables practice

---
+++ variable_practice.py
@@ -0,0 +1,22 @@
+name = input("Enter your name")
+age = input("Enter your age")
+print("Hello " + name + ", you are " + age + " years old.")
+
+x = 10
+print(10)
+
+a=5
+b=7
+print(a+b)
+
+num= int(input("Enter a number: "))
+square = num * num
+print("The square of", num, "is", square)
+
+name = input("my name is mazhar")
+age= input("my age is 22")
+print("Hello " + name + ", you are " + age + " years old.")
+
+city = input("enter your city:")
+country = input("enter your country:")
+print("you live in " + city + ", " + country)
